fd72459ef1025b52017a1c36f3a76a4bc599335b
JohnHoder <hoder.john@gmail.com>
2015-11-22 01:08:12 +0100

    Initial commit (Cloned)

---
+++ device_gio.mk
@@ -0,0 +1,37 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## Note: this file and all of its includes are parsed before any
+## BoardConfig files. 3.0 Kernel configuration must be set here.
+
+## Build the 3.0.x Kernel
+#BUILD_WITH_30X_KERNEL := false
+
+## Ramdisk
+PRODUCT_COPY_FILES += \
+    device/samsung/gio/ramdisk/GIO.rle:root/GIO.rle
+
+# Inherit products (Most specific first)
+# gio blobs > samsung common(device/vendor) > other blobs
+$(call inherit-product, vendor/samsung/gio/vendor_blobs.mk)
+$(call inherit-product, device/samsung/msm7x27-common/common.mk)
+$(call inherit-product, vendor/samsung/msm7x27-common/vendor.mk)
+
+## MDPI assets
+PRODUCT_AAPT_CONFIG := normal mdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+$(call inherit-product, device/mdpi-common/mdpi.mk)
+
+## Inherit overlays  (Most specific last)
+DEVICE_PACKAGE_OVERLAYS += device/samsung/gio/overlay
